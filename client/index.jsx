5bc19a5c5f8397c6e608880d2d5b6bfba1501bb1
毛凯露 <maokailu@dst69650.local>
2018-07-11 17:00:53 +0800

    resolve conflict

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-<<<<<<< HEAD
-import App from './comments';
-import './adaptation'
-=======
 import App from './pages/comments';
-
->>>>>>> 53e3d43196ef3718f3b0fec5f7355f860aa046b4
+import './adaptation'
 render(
     <App />,
     document.getElementById('app')
